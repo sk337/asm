ab6d225a9d4d9f1bdf437fce9eef7e489d4b8be9
sk337 <79583517+sk337@users.noreply.github.com>
2024-09-11 17:51:12 +0000

    add [as]

---
+++ [as]/src/index.asm
@@ -0,0 +1,16 @@
+section .rodata
+    as db '[as]', 0
+    as_len equ $-as
+
+section .text
+    global _start
+
+_start:
+    ; write(1, as, as_len)
+    MOV rax, 1
+    MOV rdi, 1
+    MOV rsi, as
+    MOV rdx, as_len
+    SYSCALL
+
+    JMP _start
