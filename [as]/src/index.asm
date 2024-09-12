764a162784cd5cd58bd6982866725a37d4d68615
sk337 <79583517+sk337@users.noreply.github.com>
2024-09-12 14:12:37 -0700

    Update index.asm

--- [as]/src/index.asm
+++ [as]/src/index.asm
@@ -6,12 +6,10 @@ section .text
     global _start
 
 _start:
-    ; write(1, as, as_len)
     MOV rax, 1
     MOV rdi, 1
     MOV rsi, as
     MOV rdx, as_len
     SYSCALL
 
-    ; loop forever
     JMP _start
